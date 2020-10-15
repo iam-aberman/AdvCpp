fc6f3b51d5a6303279d0a073db544b78b2e78709
Osip Chin <o.chingopin@gmail.com>
2020-10-15 23:54:33 +0300

    Created HW-1 folder
    Howework week 17

---
+++ HW-1/process.h
@@ -0,0 +1,34 @@
+//
+// Created by Osip Chin
+//
+#ifndef PROCESS_H
+#define PROCESS_H
+
+#include <string>
+#include <vector>
+#include <sys/types.h>
+
+class Process {
+public:
+    explicit Process(std::vector<std::string> arguments);
+    ~Process();
+
+    size_t write(const void* data, size_t len);
+    void writeExact(const void* data, size_t len);
+    size_t read(void* data, size_t len);
+    void readExact(void* data, size_t len);
+
+    void closeStdin();
+    void close();
+
+private:
+    void closePipes();
+
+    pid_t fork_pid_;
+
+    // Pipes
+    int parent_to_child_;
+    int child_to_parent_;
+};
+
+#endif //PROCESS_H
