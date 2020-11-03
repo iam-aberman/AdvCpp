e5575b8d80765ec7ea25aaac258575f13788f9c7
Osip Chin <o.chingopin@gmail.com>
2020-11-03 18:36:22 +0300

    Added ::waitpid(...)
    Forgot to catch the child-process

--- HW-3/test.cpp
+++ HW-3/test.cpp
@@ -1,10 +1,11 @@
 //
 // Created by Osip Chin on 30.10.2020.
 //
-#include <iostream>
-#include <unistd.h>
 #include "server.h"
 #include "connection.h"
+
+#include <iostream>
+#include <unistd.h>
 
 using namespace tcp;
 
@@ -12,7 +13,7 @@ int main() {
     std::string addr = "127.0.0.1";
     uint16_t port = 7070;
 
-    int child_pid = ::fork();
+    pid_t child_pid = ::fork();
     if (child_pid < 0) {
         std::cerr << "Fork failed!";
         return -1;
@@ -26,6 +27,12 @@ int main() {
 
         std::string message = "Hello from parent!";
         server_to_client.write(message.data(), message.size());
+
+        int status;
+        if (::waitpid(child_pid, &status, 0) < 0) {
+            std::cerr << "Smth went wrong!\n";
+            return status;
+        }
     } else {
         ::sleep(2);
         Connection client_to_server(addr, port);
@@ -33,6 +40,7 @@ int main() {
         std::string buf(1024, '\0');
         size_t read = client_to_server.read(buf.data(), buf.size());
         std::cout << "[CHILD] Message: " << buf.substr(0, read);
+        return 0;
     }
 
     return 0;
