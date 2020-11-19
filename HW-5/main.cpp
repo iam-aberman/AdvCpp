fd01780e317205da0c7e91ffab7155ae10e975aa
Osip Chin <o.chingopin@gmail.com>
2020-11-19 20:21:04 +0300

    Simple example

--- HW-5/main.cpp
+++ HW-5/main.cpp
@@ -1,7 +1,60 @@
 #include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+#include "service.h"
+#include "connection.h"
+#include "service_listener.h"
 
 
 int main(int argc, char* argv[]) {
-    std::cout << "Hello, ServiceListener!" << std::endl;
+
+    std::string address = "127.0.0.1";
+    int port = 7070;
+
+    net::Service service;
+    service.set_listener(std::make_unique<net::ExampleListener>());
+    service.open(address, port);
+
+    pid_t fork = ::fork();
+    if (fork < 0) {
+        std::cerr << "Fork failed!\n";
+        return -1;
+    }
+
+    if (fork == 0) {
+        tcp::Connection to_server(address, port);
+
+        ::sleep(2);
+
+        std::string msg = "message";
+        to_server.write(msg.data(), msg.size());
+
+        ::sleep(2);
+        std::string buf(1024, '\0');
+        {
+            size_t read = to_server.read(buf.data(), buf.size());
+            std::cout << buf.substr(0, read) << std::endl;
+        }
+
+        ::sleep(2);
+        msg = "advanced C++";
+        to_server.write(msg.data(), msg.size());
+
+        ::sleep(2);
+        {
+            size_t read = to_server.read(buf.data(), buf.size());
+            std::cout << buf.substr(0, read) << std::endl;
+        }
+
+        to_server.close();
+        return 0;
+    } else {
+        try {
+            service.run();
+        } catch (const std::exception& e) {}
+
+        ::waitpid(fork, nullptr, 0);
+    }
+
     return 0;
 }
